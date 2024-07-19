c88b33e78565c560c2d6885d9386608832046f59
Max Wright <max.wri@gmail.com>
2024-07-19 00:01:41 -0700

    Caps error

--- components/CoverMain.vue
+++ components/CoverMain.vue
@@ -49,7 +49,7 @@ export default {
   },
   data() {
     return {
-      projects: require('../assets/projects.json')
+      projects: require('~/Assets/projects.json')
     };
   },computed: {
   highlightedProjects() {
