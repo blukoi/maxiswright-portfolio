0bf0cb569f652aafd23006b9a501b2e6fe487cd1
Max Wright <max.wri@gmail.com>
2024-07-24 20:31:31 -0700

    Testing

--- pages/contact.vue
+++ pages/contact.vue
@@ -48,6 +48,7 @@
                         <button type="submit" class="button px-4 py-2 bg-grayLightPrimary hover:bg-mint text-grayDarker hover:text-white rounded-sm">Send</button>
                     </p>
                 </form>
+                <nuxt-link to="/success">Test</nuxt-link>
             </div>
         </div>
       </div>
