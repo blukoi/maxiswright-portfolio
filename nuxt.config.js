9abaf0ee1fc85dd1de2530190b872ee0d6dba450
Max Wright <max.wri@gmail.com>
2024-12-12 00:43:46 -0800

    FontAwesome bug
    • Rolling back a fontawesome change from 'mode: `client`' and pursuing other fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '@/plugins/fontawesome.js', mode: 'client' }
+    '@/plugins/fontawesome.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
