2a9e04f35cf89357613a3618015967d7537389a7
Max Wright <max.wri@gmail.com>
2024-12-17 13:09:05 -0800

    Small visual changes

--- components/CoverHome.vue
+++ components/CoverHome.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="w-full py-8">
-      <div class="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <div class="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-px">
         <nuxt-link v-for="project in highlightedProjects" id="projectContainer" :key="project.title" :to="project.buttonLink" :class="project.color" class="notch-large col-span-1 overflow-hidden">
           <div id="projectChild" class="project-thumbnail py-8 lg:py-16 px-4 sm:px-8 lg:px-16 h-full">
             <div class="w-full space-y-8 overflow-hidden flex flex-col space-y-8">
-              <h1 class="font-title title-normal text-3xl text-white flex flex-col justify-items-start">
-                <span class="w-max mb-4 h-10 px-4 flex flex-row items-center bg-white text-mint rounded-full space-x-4 z-30">
+              <h1 class="top-hash-mark-lightgray pt-1 font-title title-normal text-3xl text-white flex flex-col justify-items-start">
+                <!-- <span class="w-max mb-4 h-10 px-4 flex flex-row items-center bg-white text-mint rounded-full space-x-4 z-30">
                     <FontAwesomeIcon :icon="[project.categoryIcon.split(' ')[0], project.categoryIcon.split(' ')[1]]" class="text-lg" />
                     <p class="font-main text-sm font-bold">{{ project.category }}</p>
-                </span>
+                </span> -->
                 {{ project.title }}
               </h1>
               <div class="overflow-hidden z-20">
@@ -33,10 +33,10 @@
                 </p>
 
                 <!-- TAGS -->
-                <ul class="flex flex-row flex-wrap gap-2">
-                  <li v-for="tag in project.researchtags" :key="tag" class="bg-whiteFaded text-grayDarkest bg-opacity-50 rounded-full font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
-                  <li v-for="tag in project.designtags" :key="tag" class="bg-whiteFaded text-grayDarkest bg-opacity-50 rounded-full font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
-                  <li v-for="tag in project.impacttags" :key="tag" class="bg-whiteFaded text-grayDarkest bg-opacity-50 rounded-full font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
+                <ul class="flex flex-row flex-wrap gap-px">
+                  <li v-for="tag in project.researchtags" :key="tag" class="bg-whiteFaded text-grayDarkest bg-opacity-50 font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
+                  <li v-for="tag in project.designtags" :key="tag" class="bg-whiteFaded text-grayDarkest bg-opacity-50 font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
+                  <li v-for="tag in project.impacttags" :key="tag" class="bg-whiteFaded text-grayDarkest bg-opacity-50 font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
                 </ul>
               </div>
 
