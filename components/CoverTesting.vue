2506df6145544de7affcbad0172ce21ce747fa8f
Max Wright <max.wri@gmail.com>
2025-01-25 00:59:18 -0800

    Testing out different microinteractions

---
+++ components/CoverTesting.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="w-full">
+      <div class="h-auto">
+        <div>
+            <div class="flex flex-row space-x-px z-40">
+                <div
+                    v-for="(project, index) in highlightedProjects"
+                    :key="project.title + '-tab'"
+                    class="button notch border-black w-max h-10"
+                    :class="{
+                    'border border-b-0 notch-border bg-white text-black': index === selectedProjectIndex,
+                    'border-b bg-grayLightPrimary text-grayDarker': index !== selectedProjectIndex
+                    }"
+                    @click="selectedProjectIndex = index"
+                >
+                    <button 
+                        class="button h-10 w-20 project-tab relative flex flex-row justify-center place-items-center" 
+                        :class="{
+                        'pt-1': index === selectedProjectIndex,
+                        'pt-0 sweep-hover': index !== selectedProjectIndex
+                        }"
+                    >
+                        <FontAwesomeIcon
+                        :icon="[project.categoryIcon.split(' ')[0], project.categoryIcon.split(' ')[1]]"
+                        class="text-lg"
+                        />
+                    </button>
+                </div>
+            </div>
+
+            <!-- The Currently Selected Project's Content -->
+            <div 
+                v-if="activeProject" 
+                class="border border-black p-2 lg:p-4 bg-white z-30 -mt-px"
+            >
+                <div class="w-full h-max notch-small overflow-hidden">
+                    <transition name="wipe" mode="out-in">
+                        <div
+                            :key="activeProject.title"
+                            :class="activeProject.color" 
+                            class="project-container notch-small"
+                        >
+                            <nuxt-link
+                                :to="activeProject.buttonLink"
+                                class="project-child grid grid-cols-1 gap-8 p-8 text-white"
+                            >
+                                <h1 class="font-title title-bold text-xl">
+                                {{ activeProject.title }}
+                                </h1>
+            
+                                <!-- Media (Lottie, Video, or Image) -->
+                                <div class="overflow-hidden z-20">
+                                    <div v-if="activeProject.animation">
+                                        <LottieDynamic :animation-path="activeProject.animation" />
+                                    </div>
+            
+                                    <div v-else-if="activeProject['video-lg']">
+                                        <VideoPlayer
+                                        :videoSources="[
+                                            { src: activeProject['video-lg'], type: 'video/mp4' },
+                                            { src: activeProject['video-md'], type: 'video/mp4' },
+                                            { src: activeProject['video-sm'], type: 'video/mp4' }
+                                        ]"
+                                        />
+                                    </div>
+            
+                                    <img
+                                        v-else
+                                        :src="activeProject.image"
+                                        :alt="activeProject.title"
+                                    />
+                                </div>
+            
+                                <div class="space-y-8">
+                                    <p class="font-main text-grayLightPrimary font-sm">
+                                        {{ activeProject.caption }}
+                                    </p>
+            
+                                    <ul class="flex flex-row flex-wrap gap-px">
+                                        <li
+                                        v-for="tag in activeProject.researchtags"
+                                        :key="tag"
+                                        class="text-whiteFaded border-whiteFaded border font-main text-xs px-2.5 py-1 pb-1.5"
+                                        >
+                                        {{ tag }}
+                                        </li>
+                                        <li
+                                        v-for="tag in activeProject.designtags"
+                                        :key="tag"
+                                        class="text-whiteFaded border-whiteFaded border font-main text-xs px-2.5 py-1 pb-1.5"
+                                        >
+                                        {{ tag }}
+                                        </li>
+                                        <li
+                                        v-for="tag in activeProject.impacttags"
+                                        :key="tag"
+                                        class="text-whiteFaded border-whiteFaded border font-main text-xs px-2.5 py-1 pb-1.5"
+                                        >
+                                        {{ tag }}
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nuxt-link>
+                        </div>
+                    </transition>
+                </div>
+            </div>
+        </div>
+        <!-- <nuxt-link v-for="project in highlightedProjects" :key="project.title" :to="project.buttonLink" :class="project.color" class="notch col-span-1 overflow-hidden project-container">
+          <div class="project-thumbnail py-8 lg:py-16 px-4 sm:px-8 lg:px-16 h-full project-child">
+            <div class="w-full space-y-8 overflow-hidden flex flex-col space-y-8">
+              <h1 class="font-title title-bold text-xl text-white flex flex-col justify-items-start">
+                <span class="w-max mb-4 h-10 px-4 flex flex-row items-center bg-white text-mint rounded-full space-x-4 z-30">
+                    <FontAwesomeIcon :icon="[project.categoryIcon.split(' ')[0], project.categoryIcon.split(' ')[1]]" class="text-lg" />
+                    <p class="font-main text-sm font-bold">{{ project.category }}</p>
+                </span>
+                {{ project.title }}
+              </h1>
+              <div class="overflow-hidden z-20">
+                <div v-if="project.animation">
+                  <LottieDynamic :animation-path="project.animation"/>
+                </div>
+                <div v-else-if="project['video-lg']">
+                  <VideoPlayer
+                  :videoSources="[
+                      { src: project['video-lg'], type: 'video/mp4' },
+                      { src: project['video-md'], type: 'video/mp4' },
+                      { src: project['video-sm'], type: 'video/mp4' },
+                      ]">
+                  </VideoPlayer>
+                </div>
+                <img v-else :src="project.image" :alt="project.title">
+              </div>
+              <div class="space-y-4">
+                <p class="font-main text-grayLightPrimary font-sm">
+                    {{ project.caption }}
+                </p>
+
+                
+                <ul class="flex flex-row flex-wrap gap-px">
+                  <li v-for="tag in project.researchtags" :key="tag" class="text-whiteFaded border-whiteFaded border font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
+                  <li v-for="tag in project.designtags" :key="tag" class="text-whiteFaded border-whiteFaded border font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
+                  <li v-for="tag in project.impacttags" :key="tag" class="text-whiteFaded border-whiteFaded border font-main text-xs px-2.5 py-1 pb-1.5">{{ tag }}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nuxt-link> -->
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: require('~/Assets/projects.json'),
+      selectedProjectIndex:0
+    };
+  },
+  computed: {
+    highlightedProjects() {
+      return this.projects
+        .filter(p => p.highlight === true && p.completed === true)
+        .reverse();
+    },
+    activeProject() {
+      return this.highlightedProjects[this.selectedProjectIndex] || null;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.project-container .project-child {
+  background: linear-gradient(#000000 0 0) calc(100% - var(--p,0%)) / var(--p,0%) no-repeat;
+  transform: scale(1) skewY(0deg);
+  transition: transform 0.4s, background-size 0.4s, background-position 0s;
+}
+
+.project-container:hover .project-child {
+  transform: scale(1.05) skewY(-2deg);
+  --p: 100%;
+}
+
+/* The element transitions from a zero-width clip-path to 100% width, creating a "wipe" effect */
+
+/* 1. Control timing & easing for the clip-path property */
+.wipe-enter-active, .wipe-leave-active {
+  transition: clip-path 0.4s ease; /* Adjust duration/ease as desired */
+}
+
+/* 2. Entering element: starts fully clipped (invisible) */
+.wipe-enter {
+  clip-path: polygon(0 0, 0 0, 0 100%, 0 100%);
+}
+/* Transitioning to fully revealed state */
+.wipe-enter-to {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+}
+
+/* 3. Leaving element: starts fully visible */
+.wipe-leave {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+}
+/* Transitions back to fully clipped */
+.wipe-leave-to {
+  clip-path: polygon(100% 0, 100% 0, 100% 100%, 100% 100%);
+}
+
+.sweep-hover {
+  background: linear-gradient(#00AB8F 0 0) calc(100% - var(--p,0%)) / var(--p,0%) no-repeat;
+  transform: scale(1) rotate(0deg);
+  color: #505050;
+  border-width: 0 0 1px 0;
+  border-color: #000000;
+  transition: transform 0.4s, background-size 0.4s, background-position 0s, color 0.4s;
+}
+
+.sweep-hover:hover {
+  --p: 100%;
+  color: #FFFFFF;
+}
+</style>
---
+++ pages/testing.vue
@@ -0,0 +1,116 @@
+<template>
+    <div>
+      <CustomCursor />
+      <NavBar />
+      <!-- <TestComponent /> -->
+      <div class="w-full content-center z-10">
+        <div class="container px-4 sm:px-0 mx-auto pt-32">
+  
+          <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-x-0 gap-y-16">
+  
+            <!-- <div class="z-40 lg:pt-24 lg:pb-24 static col-span-1 lg:col-span-2 col-start-1 lg:col-start-2 flex flex-row justify-center mx-0 lg:mx-24 xl:mx-32 2xl:mx-56">
+              <JumbotronHome class="z-0 w-full md:w-1/2 lg:w-full" />
+            </div> -->
+  
+            <!-- <div class="col-span-1 lg:col-span-3 w-max bg-mint text-white h-16 px-8 items-center rounded-full flex flex-row gap-4">
+              <div class="h-8 w-8 flex flex-row justify-center items-center">
+                <FontAwesomeIcon id="WavingHand" :icon="['fas', 'hand']" class="text-3xl" />
+              </div>
+              <h1 class="font-title title-bold text-3xl pt-1">
+                Hi!
+              </h1>
+            </div> -->
+  
+            <div class="col-span-1 col-start-1 lg:col-span-2 justify-center">
+                <div class="space-y-6 lg:space-y-12 pl-8">
+                    <div class="w-max bracketing bracket-tl bracket-br bracket-tl-thick bracket-br-thick bg-transparent text-black font-title text-xl title-bold px-4 pt-2 pb-1">
+                    <FontAwesomeIcon id="WavingHand" :icon="['fas', 'hand']" class="text-xl text-mint" /><span class="pl-4">Hello!</span>
+                    </div>
+                    <p class="py-6 font-normal font-main text-xl text-grayMiddle introduction">
+                    As a mission-driven product designer & exhaustive explorer, I excel at applying insights to interfaces to craft scalable design systems & intuitive microinteractions.
+                    </p>
+                    <!-- <p class="font-normal font-main text-grayMiddle">
+                    My experience with visual communication, front-end development, and working as a freelancer alongside engineers & stakeholders helps me quickly impact projects as a pro-active collaborator.
+                    </p> -->
+                    <!-- <p class="font-normal font-main text-grayMiddle">
+                    Currently contributing to user research, design systems, and UI design at Keyworded.
+                    </p> -->
+                </div>
+  
+                <div class="justify-start flex flex-row items-center space-x-2 mt-6 lg:mt-12">
+                <!-- <WorkAvailability /> -->
+                    <div class="w-6 text-mint text-2xl">
+                        <font-awesome-icon :icon="['fas', 'arrow-right-long']" />
+                    </div>
+                    <ButtonLight label="Let's Talk" link="/contact" class="content-center pointer-events-auto w-max" />
+                </div>
+            </div>
+  
+            <div class="col-span-1 col-start-1 lg:col-start-2 space-y-10">
+                <div class="w-full text-right">
+                    <h1 class="font-title title-normal text-grayLighter text-6xl">Highlights —</h1>
+                </div>
+                <CoverTesting />   
+            </div>
+        
+          </div>
+  
+        </div>
+      </div>
+      <PreFooter :currentProjectId="'1'" :linksVisibility="[false, true, true, false]" />
+      <FooterSection />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    head: {
+      title: 'Max is Wright',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: "Max's new, rebuilt portfolio website. Max is a versatile product designer with a talent for visual design and product + systems thinking. He's eager to tackle complex challenges and craft user-focused digital experiences. This website was built with plain HTML, CSS, and JS, and ChatGPT was used to convert the entire site for Vue.JS and Nuxt.JS, including project initiation, folder management, using terminal, script changes, incorporating JSON, and problem fixes."
+        }
+      ],
+    },
+  }
+  </script>
+  
+  <style>
+  @keyframes wave-animation {
+      0%, 20%, 80%, 100% {
+          transform: scaleX(-1) rotate(0deg);
+      }
+      40% {
+          transform: scaleX(-1) rotate(-20deg);
+      }
+      60% {
+          transform: scaleX(-1) rotate(20deg);
+      }
+  }
+  
+  #WavingHand {
+      display: inline-block;
+      animation: wave-animation 3s ease-in-out infinite;
+      transform-origin: bottom;
+  }
+  
+  #jobTitle {
+    text-decoration: underline solid #00AB8F;
+    text-decoration-thickness: 4px;
+    text-underline-offset: 2px;
+    text-decoration-skip-ink: none;
+  }
+  
+  /* .fa, .fas, .far, .fal, .fad, .fab {
+      min-width: 1em;
+      min-height: 1em;
+  } */
+
+  .introduction {
+    max-width: 70ch;
+    overflow-wrap: break-word;
+  }
+  </style>
